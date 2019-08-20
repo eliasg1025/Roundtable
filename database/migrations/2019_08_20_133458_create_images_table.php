b14de19bc9c47f12634071b3b5ec995a969b6213
Elias Guere <eliasguere1025@gmail.com>
2019-08-20 14:09:16 -0500

    Migrando tablas de la logica del negocio

---
+++ database/migrations/2019_08_20_133458_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('url');
+            $table->timestamps();
+            //Fks
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->bigInteger('operation_id')->unsigned();
+            $table->foreign('operation_id')->references('id')->on('operations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
