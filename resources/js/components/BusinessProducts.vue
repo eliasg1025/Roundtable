efebdfe3377881a6c35644cd2f5f014ccf8212ed
Elias Guere <eliasguere1025@gmail.com>
2019-09-07 01:03:05 -0500

    Renombrando los directorios del proyecto

--- resources/js/components/BusinessProducts.vue
+++ resources/js/components/BusinessProducts.vue
@@ -6,7 +6,7 @@
 
 <script>
 	export default {
-		name: 'business-product',
+		name: 'business-products',
 	}
 </script>
 
--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-	<inicio
+	<home
 		:data="{{ json_encode($data) }}"
 		csrf="{{ csrf_token() }}"
 		href_login="{{ route('login') }}"
 		href_register="{{ route('register') }}"
 		href_reset_password="{{ route('password.request') }}"
-	/>
+	>
+	</home>
 @endsection
