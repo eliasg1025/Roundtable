b70e1798618679f3d5bf99844cc6f82e8011ae4d
Elias Guere <eliasguere1025@gmail.com>
2019-09-12 17:10:40 -0500

    Guardando cambios

--- public/js/app.js
+++ public/js/app.js
@@ -61749,8 +61749,8 @@ $('#formLogin > div.content > div.form-check > #pass-check').click(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/elias/Documents/php-projects/roundtable/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/elias/Documents/php-projects/roundtable/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/elias/Documents/projects/roundtable/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/elias/Documents/projects/roundtable/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
