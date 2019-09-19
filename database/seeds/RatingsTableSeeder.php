048356fe15fd56813463d3a99dd18fe793cf53f1
Elias Guere <eliasguere1025@gmail.com>
2019-09-18 22:24:57 -0500

    Arreglando errores de registro de cuenta
    En el momento que se activaron los observers para la creacion de usuarios se
    activaba la asignacion de planes y el rating, pero al momento de registrar un
    usuario mediante el formulario no se activaban, se soluciono implementando un
    Listener en el momento que se verificaba el email y que asignara el plan free y
    un rating inicial de 3.

--- database/seeds/RatingsTableSeeder.php
+++ database/seeds/RatingsTableSeeder.php
@@ -14,42 +14,67 @@ class RatingsTableSeeder extends Seeder
     {
 		$rating = new Rating();
 		$rating->value = 4;
-		$rating->user_id = 30;
+		$rating->user_id = 1;
 		$rating->save();
 
 		$rating = new Rating();
 		$rating->value = 4;
-		$rating->user_id = 30;
+		$rating->user_id = 1;
 		$rating->save();
 
 		$rating = new Rating();
 		$rating->value = 4;
-		$rating->user_id = 30;
+		$rating->user_id = 1;
 		$rating->save();
 
 		$rating = new Rating();
 		$rating->value = 3;
-		$rating->user_id = 30;
+		$rating->user_id = 1;
 		$rating->save();
 
 		$rating = new Rating();
 		$rating->value = 5;
-		$rating->user_id = 30;
+		$rating->user_id = 1;
 		$rating->save();
 
 		$rating = new Rating();
 		$rating->value = 5;
-		$rating->user_id = 30;
+		$rating->user_id = 1;
 		$rating->save();
 
 		$rating = new Rating();
 		$rating->value = 4;
-		$rating->user_id = 30;
+		$rating->user_id = 1;
 		$rating->save();
 
 		$rating = new Rating();
 		$rating->value = 3;
-		$rating->user_id = 30;
+		$rating->user_id = 1;
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->value = 2;
+		$rating->user_id = 1;
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->value = 3;
+		$rating->user_id = 2;
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->value = 3;
+		$rating->user_id = 3;
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->value = 3;
+		$rating->user_id = 4;
+		$rating->save();
+
+		$rating = new Rating();
+		$rating->value = 3;
+		$rating->user_id = 5;
 		$rating->save();
     }
 }
