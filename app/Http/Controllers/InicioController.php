110c400f69d53275ce93e73cf75ba0eee05d494c
Elias Guere <eliasguere1025@gmail.com>
2019-10-28 18:38:46 -0500

    En la pagina de inicio solo aparecen los usuarios con el mejor plan

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -9,108 +9,107 @@ use Illuminate\Support\Facades\DB;
 
 class InicioController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        /* $this->middleware('auth'); */
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		/* $this->middleware('auth'); */
+	}
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
+	/**
+	 * Show the application dashboard.
+	 *
+	 * @return \Illuminate\Contracts\Support\Renderable
+	 */
+	public function index()
+	{
 		$loged = !empty(Auth::user()) ? true : false;
 		
 		$data = [
-            'featuredSellers' => $this->getSellers(),
-            'featuredBuyers' => $this->getBuyers(),
-            'mostViewedCompanies' => $this->getMostViewedCompanies(),
+			'featuredSellers' => $this->getFeaturedUsers('seller'),
+			'featuredBuyers' => $this->getFeaturedUsers('buyer'),
+			'mostViewedCompanies' => $this->getMostViewedCompanies(),
 			'categories' => $this->getCategories(),
 			'loged' => $loged,
 		];
 		
-        return view('inicio', compact('data'));
-    }
+		return view('inicio', compact('data'));
+	}
 
-    private function getBuyers()
-    {
-        $ratingInfo = DB::table('ratings')
-                            ->select('user_id', DB::raw('COUNT(*) as amount_rating, SUM(value) as total_rating, AVG(value) as avg_rating'))
-                            ->groupBy('user_id');
-        
-        $users = App\User::select('id', 'commercial_name', 'uuid', 'profile_img', 'cover_img', 'views', 'verified', 'type_id', 'amount_rating', 'total_rating', 'avg_rating')
-                            ->joinSub($ratingInfo, 'rating_info', function($join) {
-                                $join->on('users.id', '=', 'rating_info.user_id');
-                            })
-                            ->where('type_id', 3)
-                            ->orderBy('total_rating', 'DESC', 'avg_rating', 'DESC')
-                            ->limit(10)
-                            ->get();
-        
-        return $users;
-    }
+	private function getFeaturedUsers(String $type_user)
+	{
+		switch ($type_user) {
+			case 'buyer':
+				$types = [3];
+				break;
+			
+			case 'seller':
+				$types = [1, 2];
+				break;
+		}
 
-    private function getSellers()
-    {
 		$ratingInfo = DB::table('ratings')
-                            ->select('user_id', DB::raw('COUNT(*) as amount_rating, SUM(value) as total_rating, AVG(value) as avg_rating'))
-                            ->groupBy('user_id');
-        
-        $users = App\User::select('id', 'commercial_name', 'uuid', 'profile_img', 'cover_img', 'views', 'verified', 'type_id', 'amount_rating', 'total_rating', 'avg_rating')
-                            ->joinSub($ratingInfo, 'rating_info', function($join) {
-                                $join->on('users.id', '=', 'rating_info.user_id');
-                            })
-                            ->where('type_id', 1)
-                            ->orWhere('type_id', 2)
-                            ->orderBy('total_rating', 'DESC', 'avg_rating', 'DESC')
-                            ->limit(10)
-                            ->get();
+							->select('user_id', DB::raw('COUNT(*) as amount_rating, SUM(value) as total_rating, AVG(value) as avg_rating'))
+							->groupBy('user_id');
 
-        return $users;
-    }
+		$planInfo = DB::table('plan_user')
+							->select('user_id')
+							->join('plans', 'plans.id', '=', 'plan_user.plan_id')
+							->where('plans.is_best', '=', '1');
+		
+		$users = App\User::select('id', 'commercial_name', 'uuid', 'profile_img', 'cover_img', 'views', 'verified', 'type_id', 'amount_rating', 'total_rating', 'avg_rating')
+							->joinSub($ratingInfo, 'rating_info', function($join) {
+								$join->on('users.id', '=', 'rating_info.user_id');
+							})
+							->joinSub($planInfo, 'plan_info', function($join) {
+								$join->on('plan_info.user_id', '=', 'users.id');
+							})
+							->whereIn('type_id', $types)
+							->orderBy('total_rating', 'DESC', 'avg_rating', 'DESC')
+							->limit(10)
+							->get();
+		
+		return $users;
+	}
 
-    private function getMostViewedCompanies()
-    {
-        $ratingInfo = DB::table('ratings')
-                            ->select('user_id', DB::raw('COUNT(*) as amount_rating, SUM(value) as total_rating, AVG(value) as avg_rating'))
-                            ->groupBy('user_id');
-        
-        $users = App\User::select('id', 'commercial_name', 'uuid', 'profile_img', 'cover_img', 'views', 'verified', 'type_id', 'amount_rating', 'total_rating', 'avg_rating')
-                            ->joinSub($ratingInfo, 'rating_info', function($join) {
-                                $join->on('users.id', '=', 'rating_info.user_id');
-                            })
-                            ->orderBy('views', 'DESC')
-                            ->limit(10)
-                            ->get();
+	private function getMostViewedCompanies()
+	{
+		$ratingInfo = DB::table('ratings')
+							->select('user_id', DB::raw('COUNT(*) as amount_rating, SUM(value) as total_rating, AVG(value) as avg_rating'))
+							->groupBy('user_id');
+		
+		$users = App\User::select('id', 'commercial_name', 'uuid', 'profile_img', 'cover_img', 'views', 'verified', 'type_id', 'amount_rating', 'total_rating', 'avg_rating')
+							->joinSub($ratingInfo, 'rating_info', function($join) {
+								$join->on('users.id', '=', 'rating_info.user_id');
+							})
+							->orderBy('views', 'DESC')
+							->limit(10)
+							->get();
 
-        return $users;
-    }
+		return $users;
+	}
 
-    private function getCategories()
-    {
-        $categories = App\Category::all();
-        return $categories;
+	private function getCategories()
+	{
+		$categories = App\Category::all();
+		return $categories;
 	}
 	
-    
-    /* 
-    SELECT users.id, users.commercial_name, users.uuid, users.profile_img, users.cover_img, users.views, users.verified, users.type_id, total.total_rating, total.avg_rating
-    FROM users
-    INNER JOIN
-        (
-            SELECT SUM(ratings.value) AS total_rating, AVG(ratings.value) AS avg_rating, user_id
-            FROM `ratings`
-            GROUP BY user_id
-        ) AS total
-    ON users.id = total.user_id
-    WHERE users.type_id = 1
-    ORDER BY total.total_rating DESC, total.avg_rating DESC
-    LIMIT 10 */
+	
+	/* 
+	SELECT users.id, users.commercial_name, users.uuid, users.profile_img, users.cover_img, users.views, users.verified, users.type_id, total.total_rating, total.avg_rating
+	FROM users
+	INNER JOIN
+		(
+			SELECT SUM(ratings.value) AS total_rating, AVG(ratings.value) AS avg_rating, user_id
+			FROM `ratings`
+			GROUP BY user_id
+		) AS total
+	ON users.id = total.user_id
+	WHERE users.type_id = 1
+	ORDER BY total.total_rating DESC, total.avg_rating DESC
+	LIMIT 10 */
 }
