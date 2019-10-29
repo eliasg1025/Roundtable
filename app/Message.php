511fcb4134f8f8585dc0bcf941b76f86b028dc06
Elias Guere <eliasguere1025@gmail.com>
2019-10-29 02:05:11 -0500

    Agregando mensaje de bienvenida

--- app/Message.php
+++ app/Message.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    protected $dates = ['date'];
+	protected $dates = ['date'];
+	
+	protected $fillable = [
+		'title',
+		'message',
+		'viewed',
+		'date',
+		'user_id',
+		'type_message_id',
+	];
 
     public function type_message() {
     	return $this->belongsTo(TypeMessage::class);
