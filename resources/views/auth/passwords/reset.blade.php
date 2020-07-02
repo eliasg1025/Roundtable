ae8717fb4c6c1d228be1b285c3a4ffae2829ab00
Elias Guere <eliasguere1025@gmail.com>
2020-07-02 01:19:10 -0500

    Send observaciones

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-<div class="container">
+<page-banner title="Restablecer contraseña" img="/img/fondo/fondo-pago.jpg"></page-banner>
+<div class="container" style="padding: 60px 10px;">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
@@ -15,7 +15,7 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electrónico') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
@@ -29,7 +29,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -43,7 +43,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -52,9 +52,13 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
+								<button 
+									type="submit"
+									class="btn"
+									style="color: #fff; background-color: #88be2e; border-color: #88be2e;"
+								>
+									{{ __('Reset Password') }}
+								</button>
                             </div>
                         </div>
                     </form>
@@ -63,4 +67,11 @@
         </div>
     </div>
 </div>
+<page-footer></page-footer>
+<modal-login-register
+	csrf="{{ csrf_token() }}"
+	href_login="{{ route('login') }}"
+	href_register="{{ route('register') }}"
+	href_reset_password="{{ route('password.request') }}"
+/>
 @endsection
